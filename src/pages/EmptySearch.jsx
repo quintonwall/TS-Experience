4e86ac759d128d6f9770a761457cf6bdecbef937
quintonwall <quinton_wall@hotmail.com>
2022-11-30 08:58:37 -0800

    pendo test

--- src/pages/EmptySearch.jsx
+++ src/pages/EmptySearch.jsx
@@ -4,8 +4,19 @@ import { EmbedEvent, Action, HostEvent } from '@thoughtspot/visual-embed-sdk';
 import "../config";
 import { gbl_disabledActionSet, gbl_disabledMessage } from "../config";
 
+
+function init_pendo(){
+  //embedRef.contentWindow.getElementsByName('ThoughtSpot Embedded Analytics')[0].contentWindow.pendo.initialize();
+  //useEmbedRef().pendo.initialize();
+  //useEmbedRef().current.getElementById('ThoughtSpot Embedded Analytics').contentWindow.pendo.initialize();
+  //useEmbedRef().
+  
+}
+
 const EmptySearch = () => {
   const embedRef = useEmbedRef();
+  embedRef.contentWindow.pendo.initialize();
+  
 
   return (
     <div>
