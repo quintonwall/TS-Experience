8f1ac5e12e8a60ee3338d849e951d23860ac14b8
quintonwall <quinton_wall@hotmail.com>
2022-11-30 09:21:33 -0800

    pendo testing

--- src/pages/EmptySearch.jsx
+++ src/pages/EmptySearch.jsx
@@ -5,7 +5,7 @@ import "../config";
 import { gbl_disabledActionSet, gbl_disabledMessage } from "../config";
 
 
-function init_pendo(){
+function testa(){
   //embedRef.contentWindow.getElementsByName('ThoughtSpot Embedded Analytics')[0].contentWindow.pendo.initialize();
   //useEmbedRef().pendo.initialize();
   //useEmbedRef().current.getElementById('ThoughtSpot Embedded Analytics').contentWindow.pendo.initialize();
@@ -13,20 +13,25 @@ function init_pendo(){
   
 }
 
+const init_pendo = (e) => {
+  console.log("Initializing Pendo");
+  embedRef.contentWindow.pendo.initialize();
+};
+
 const EmptySearch = () => {
   const embedRef = useEmbedRef();
-  embedRef.contentWindow.pendo.initialize();
-  
 
   return (
     <div>
       <SearchEmbed
         ref={embedRef}
+        onLoad={init_pendo}
         frameParams={{ hideDataSources: "true", height: "80vw" }}
         dataSources={"5f791f79-d175-44b4-bd67-9e748f3dfbba"}
         disabledActions={gbl_disabledActionSet}
         disabledActionReason={gbl_disabledMessage}
       />
+      
     </div>
   );
 };
