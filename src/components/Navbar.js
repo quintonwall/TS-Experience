884a67baebb305ab053917b2604191a0e5734b12
Jorenm <jorenm@gmail.com>
2023-01-31 16:51:21 -0800

    Button and nav bar styling changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -21,24 +21,25 @@ const Navbar = () => {
     
     <>
       <Nav>
-        <Bars onClick={()=>{
-          
-          navigate("/preview/home");
-      }} />
-        <NavMenu>
-        <ThoughtSpotLogo style={{marginLeft: + '-45'}} />
-          <NavLink to="/preview/liveboard">
-            LIVEBOARD
-          </NavLink>
-          <NavLink to="/preview/search" >
-            SEARCH
-          </NavLink>
-          <a class="nav" href="https://codesandbox.io/p/github/thoughtspot/TS-Experience/main">SEE THE CODE </a>
-        </NavMenu>
+        <div class="column" style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+          <Bars onClick={()=>{
+            navigate("/preview/home");
+          }} />
+          <NavMenu style={{display: 'flex', alignItems: 'center'}}>
+            <ThoughtSpotLogo style={{marginLeft: + '-45'}} />
+              <NavLink to="/preview/liveboard">
+                LIVEBOARD
+              </NavLink>
+              <NavLink to="/preview/search" >
+                SEARCH
+              </NavLink>
+              <a class="nav" href="https://codesandbox.io/p/github/thoughtspot/TS-Experience/main">SEE THE CODE </a>
+          </NavMenu>
 
-        <NavBtn style={{marginRight:+ '20'}}>
-          <a class="bluebutton" href="https://www.thoughtspot.com/trial?tsref=producttour"  target="_blank">FREE TRIAL</a>
-         </NavBtn>
+          <NavBtn style={{marginRight:+ '20'}}>
+            <a class="bluebutton" href="https://www.thoughtspot.com/trial?tsref=producttour"  target="_blank">FREE TRIAL</a>
+          </NavBtn>
+        </div>
       </Nav>
     </>
   );
