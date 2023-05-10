7506c958318929c4b97d12e4f6aa7f3e48e6f156
Michael Ye <102629468+michaelbtye@users.noreply.github.com>
2023-05-10 16:28:16 -0700

    Update Search.jsx
    Restored old settings

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -4,43 +4,21 @@ import { EmbedEvent, Action, HostEvent } from '@thoughtspot/visual-embed-sdk';
 import "../config";
 import { gbl_disabledActionSet, gbl_disabledMessage, gbl_visibleActionSet } from "../config";
 
-const Search = {
-  frameParams: {},
-  /*param-start-collapseDataSources*//*param-end-collapseDataSources*/
-  /*param-start-hideDataSources*//*param-end-hideDataSources*/
-  /*param-start-hideResults*/
-  hideResults: true,
-  /*param-end-hideResults*/
-  /*param-start-enableSearchAssist*//*param-end-enableSearchAssist*/
-  /*param-start-modifyActions*//*param-end-modifyActions*/
-  /*param-start-disabledActions*//*param-end-disabledActions*/
-  /*param-start-dataSources*//*param-end-dataSources*/
-  /*param-start-searchOptions*//*param-end-searchOptions*/
-  /*param-start-answerId*//*param-end-answerId*/
+const Search = () => {
+  const embedRef = useEmbedRef();
+
+  return (
+    <div>
+      <SearchEmbed
+        ref={embedRef}
+        frameParams={{ hideDataSources: "true", width: '100%', height: '100%', }}
+        answerId={"ef625725-b6fa-4ce4-9522-d942499a1690"}
+        disabledActions={gbl_disabledActionSet}
+        disabledActionReason={gbl_disabledMessage}
+        visibleActions={gbl_visibleActionSet}
+      />
+    </div>
+  );
 };
-
-
-// Function to show/hide
-function setDisplayStyle(el, style) {
-  if(document.getElementById(el)) {
-    document.getElementById(el).style.display = style;
-  }
-}
-
-// Functions to show and hide a loader while iframe loads
-function showLoader() {
-  setDisplayStyle("loader", "block");
-}
-function hideLoader() {
-  setDisplayStyle("loader", "none");
-}
-
-function showAuthExpired() {
-  setDisplayStyle("authExpiredBanner", "flex");
-}
-
-export default Search;
-
-
 
 export default Search;
